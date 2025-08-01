1f7799df6d01e4208549e78494bdf03816edc422
Pham Van Nhu Quynh <vannhuquynhp@gmail.com>
2025-08-01 17:17:28 +0700

    feat: add new config application.properties

---
+++ psgp_be/src/test/java/com/exe201/group1/psgp_be/PsgpBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exe201.group1.psgp_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsgpBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
