d4d7e06135157d6b41aeb189f50ae25d4c0418c6
TriTVH <huatri2004@gmail.com>
2025-08-29 10:24:26 +0700

    Get Profile Account Buyer, List Accounts Buyers, List Banned account Buyers, List Active Account Buyers

---
+++ psgp_be/src/main/java/com/exe201/group1/psgp_be/services/AdminService.java
@@ -0,0 +1,11 @@
+package com.exe201.group1.psgp_be.services;
+
+import com.exe201.group1.psgp_be.dto.response.ResponseObject;
+import org.springframework.http.ResponseEntity;
+
+public interface AdminService {
+     ResponseEntity<ResponseObject> getAllBuyerAccounts();
+     ResponseEntity<ResponseObject> getActiveBuyerAccounts();
+     ResponseEntity<ResponseObject> getBannedBuyerAccounts();
+     ResponseEntity<ResponseObject> getProfileBuyerAccount(int id);
+}
