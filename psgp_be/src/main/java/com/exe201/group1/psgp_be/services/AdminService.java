84607b6643aed3eaa9df885fc3ee7ce969c28024
TriTVH <huatri2004@gmail.com>
2025-08-29 11:17:26 +0700

    feat: feat add new function Get Profile Account Buyer, List Accounts Buyers, List Banned account Buyers, List Active Account Buyers

--- psgp_be/src/main/java/com/exe201/group1/psgp_be/services/AdminService.java
+++ psgp_be/src/main/java/com/exe201/group1/psgp_be/services/AdminService.java
@@ -1,5 +1,6 @@
 package com.exe201.group1.psgp_be.services;
 
+import com.exe201.group1.psgp_be.dto.requests.ProcessAccountRequest;
 import com.exe201.group1.psgp_be.dto.response.ResponseObject;
 import org.springframework.http.ResponseEntity;
 
@@ -8,4 +9,6 @@ public interface AdminService {
      ResponseEntity<ResponseObject> getActiveBuyerAccounts();
      ResponseEntity<ResponseObject> getBannedBuyerAccounts();
      ResponseEntity<ResponseObject> getProfileBuyerAccount(int id);
+     ResponseEntity<ResponseObject> banBuyerAccount(ProcessAccountRequest request);
+     ResponseEntity<ResponseObject> unbanBuyerAccount(ProcessAccountRequest request);
 }
