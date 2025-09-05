38d76f2784934e545949c0cfef4b250c6b188921
Long Pham <longpham5252@gmail.com>
2025-09-05 11:26:42 +0700

    fix: change ws endpoint

--- psgp_be/src/main/java/com/exe201/group1/psgp_be/configs/WebSocketConfig.java
+++ psgp_be/src/main/java/com/exe201/group1/psgp_be/configs/WebSocketConfig.java
@@ -15,6 +15,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
+        registry.addEndpoint("/ws-endpoint").setAllowedOriginPatterns("*").withSockJS();
     }
 }
