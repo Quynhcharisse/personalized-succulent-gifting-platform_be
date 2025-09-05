9a1d8cd33d3f4fa5adfa9ac4570b5f5c72e72c18
Long Pham <longpham5252@gmail.com>
2025-09-05 11:22:02 +0700

    update: get notifications

--- psgp_be/src/main/java/com/exe201/group1/psgp_be/controllers/NotificationController.java
+++ psgp_be/src/main/java/com/exe201/group1/psgp_be/controllers/NotificationController.java
@@ -1,22 +1,30 @@
 package com.exe201.group1.psgp_be.controllers;
 
+import com.exe201.group1.psgp_be.dto.requests.CreateNotificationRequest;
+import com.exe201.group1.psgp_be.dto.response.ResponseObject;
 import com.exe201.group1.psgp_be.models.Notification;
 import com.exe201.group1.psgp_be.services.NotificationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/notifications")
+@RequestMapping("/api/v1/notifications")
 public class NotificationController {
     private final NotificationService notificationService;
     private final SimpMessagingTemplate messagingTemplate;
 
     @PostMapping("/send")
-    public Notification sendNotification(@RequestParam Integer accountId, @RequestParam String message) {
-        Notification notification = notificationService.createNotification(accountId, message);
-        messagingTemplate.convertAndSend("/topic/notifications/" + accountId, notification);
-        return notification;
+    public ResponseEntity<ResponseObject> sendNotification(@RequestBody CreateNotificationRequest request) {
+        ResponseEntity<ResponseObject> response = notificationService.createNotification(request);
+        messagingTemplate.convertAndSend("/topic/notifications/" + request.getAccountId(), response);
+        return response;
+    }
+
+    @GetMapping("/{accountId}")
+    public ResponseEntity<ResponseObject> getNotifications(@PathVariable Integer accountId) {
+        return notificationService.getAccountNotifications(accountId);
     }
 }
